a8102ed7dee2ce0f28768ddf18c403050a68e4d1
Vikas Rathore <vikasrathore@cedcoss.com>
2022-03-02 19:02:09 +0530

    updating singup and login page

--- src/templates/Signup.php
+++ src/templates/Signup.php
@@ -18,29 +18,30 @@
         <img class="mb-4 bg-dark" src="../imgs/cedcoss-logo.png" alt="" width="300" height="90">
         <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
 
-
-        <div class="form-floating">
+        <div class="form-floating mb-2">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
           <label for="floatingInput">Username</label>
         </div>
-        <div class="form-floating">
+        <div class="form-floating mb-2">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingInput">Phone Number</label>
+          <label for="floatingInput">First Name</label>
         </div>
-        <div class="form-floating">
+        <div class="form-floating mb-2">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Last Name</label>
+        </div>
+        <div class="form-floating mb-2">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
           <label for="floatingInput">Email address</label>
         </div>
-        <div class="form-floating">
+        <div class="form-floating mb-2">
           <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
           <label for="floatingPassword">Password</label>
         </div>
     
-        <div class="checkbox mb-3">
-        
-        </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+       
+        <button class="w-75 btn btn-lg btn-primary" type="submit">Sign Up</button>
+        <p class="mt-5 mb-3 text-muted">&copy; Cedcoss - 2022 </p>
       </form>
     </main>
 
