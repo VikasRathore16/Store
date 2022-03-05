24fadd8c23e3e62826518736fd1d7ad8fadf1035
Vikas Rathore <vikasrathore@cedcoss.com>
2022-03-05 11:57:21 +0530

    store

--- src/index.php
+++ src/index.php
@@ -1,9 +1,7 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once 'vendor/autoload.php'; ?>
 
-?>
-  
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,37 +17,63 @@ require_once "vendor/autoload.php";
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="">
-            <img src="../assests/imgs/cedcoss-logo.png" alt="Avatar Logo"  class="w-25"> 
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="mynavbar">
-            <ul class="navbar-nav me-auto">
-              <!-- <li class="nav-item">
-                <a class="nav-link text-light border" href="admin/index.php">Log In</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light border" href="admin/Signup.php">Sign Up</a>
-              </li> -->
-              <!-- <li class="nav-item">
-                <a class="nav-link text-light" href="admin/dashboard.php">Dashboard</a>
-              </li> -->
-            <li class="nav-item me-4">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="">
+        <img src="../assests/imgs/cedcoss-logo.png" alt="Avatar Logo" class="w-25">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item me-4">
             <form class="d-flex">
               <input class="form-control me-2" type="text" placeholder="Search">
               <button class="btn btn-primary" type="button">Search</button>
             </form>
-            </li>
-            <li class="nav-item float-end">
-                <a class="nav-link text-light border" href="admin/index.php">Log In</a>
-              </li>
-            </ul>
+          </li>
+          <li class="nav-item float-end">
+            <a class="nav-link text-light border" href="admin/index.php">Log In</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="product-big-title-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="product-bit-title text-center">
+            <h2>Shop</h2>
           </div>
         </div>
-      </nav>
+      </div>
+    </div>
+  </div>
+  <div class="single-product-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="img/product-2.jpg" alt="">
+                        </div>
+                        <h2><a href="">Apple new mac book 2015 March :P</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$899.00</ins> <del>$999.00</del>
+                        </div>  
+                        
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                        </div>                       
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
 </body>
+
 </html>
